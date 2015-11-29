80bd6c2417843ada3746444993637ecc6046256b
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-11-29 19:51:26 +0100

    Etape2

--- charon/etape2/Function.hpp
+++ charon/etape2/Function.hpp
@@ -5,42 +5,94 @@
 // Login   <nicolaschr@epitech.net>
 //
 // Started on  Tue Nov 24 00:12:08 2015 Nicolas Charvoz
-// Last update Tue Nov 24 00:12:46 2015 Nicolas Charvoz
+// Last update Sun Nov 29 19:50:29 2015 Nicolas Charvoz
 //
 
 #ifndef FUNCTION_HPP_
-# define FUNCTION_HPP_
+#define FUNCTION_HPP_
 
-template < typename T >
+template <class T>
 
 class Function;
 
-template <typename T, typename U>
+template<class T, class U>
 
-class Function < T(U) >
+class Function <T(U)>
 {
+private:
+  typedef T (*type)(U);
+  type _func;
+
 public:
+  Function() : _other(0) {}
+  Function(T (*p)(U)) : _func(p), _other(0) {}
 
-  Function(T);
-  Function(T  (*f)(U)) {
-    _func = f;
+  ~Function() {
+    if (_other)
+      delete _other;
   }
-  ~Function(){}
 
-  Function operator=(T (*t)(U))
+  Function(Function<T> &f) {
+    this = f;
+  }
+
+  Function operator=(Function<T> &f)
   {
+    this->_func = f.func;
+    this->_other = f._other;
+    return this;
+  }
+
+  Function operator=(T (*t)(U)) {
     _func = t;
     return this;
   }
 
-  T operator()(U t)
+  class Herited
   {
+  public:
+    virtual T operator()(const U& t) = 0;
+    virtual ~Herited() {}
+  };
+
+  template <class I>
+
+  class Reserve : public Herited
+  {
+
+  public:
+    Reserve(const I& obj) : _obj(new I(obj)) {}
+    ~Reserve() {
+      if (_obj) delete _obj;
+    }
+
+    virtual T operator()(const U& t)
+    {
+      if (_obj)
+	return _obj->operator()(t);
+    }
+
+  private:
+
+    I *_obj;
+  };
+
+  template < class I >
+
+  Function(const I& obj)
+  {
+    _other = new Reserve<I>(obj);
+  }
+
+  T operator()(U t){
+    if (_other)
+      return _other->operator()(t);
     return _func(t);
   }
 
-protected:
-  typedef T  (*type)(U);
-  type _func;
+private:
+  Herited*_other;
+
 };
 
 #endif
--- charon/etape2/main.cpp
+++ charon/etape2/main.cpp
@@ -5,25 +5,61 @@
 // Login   <nicolaschr@epitech.net>
 //
 // Started on  Tue Nov 24 00:13:23 2015 Nicolas Charvoz
-// Last update Tue Nov 24 00:13:59 2015 Nicolas Charvoz
+// Last update Sun Nov 29 19:47:55 2015 Nicolas Charvoz
 //
 
-#include "Function.hpp"
 #include <iostream>
+#include "Function.hpp"
 #include <string>
+#include <boost/bind.hpp>
 
 
-int test1(char c)
+int func(const std::string& toto)
 {
-  std::cout << "test 1 : " <<  c << std::endl;
+  std::cout << toto << std::endl;
+  return 0;
 }
+
+int func2(const std::string& toto, int i)
+{
+  std::cout << toto << "et un int : " << i << std::endl;
+  return 0;
+}
+
+int func3(const std::string& toto, int i, const std::string& str)
+{
+  std::cout << toto << "et un int : " << i << str << std::endl;
+  return 0;
+}
+
+int func4(const std::string& toto, int i, const std::string& str, const std::string& str2)
+{
+  std::cout << toto << "et un int : " << i << str << str2 << std::endl;
+  return 0;
+}
+
+class Toto {
+public:
+  Toto() {}
+
+  char operator()(char c) {
+    std::cout << "Ça marche sur un objet callable : " << c << std::endl;
+  }
+
+private:
+
+};
 
 int main(int ac, char **av)
 {
-  (void) ac;
-  (void) av;
-  Function<int(char)> firstTest = &test1;
+  Function<int (const std::string&)> f = &func;
+  f("coucou");
 
-  firstTest('c');
-  return (0);
+  Toto toto;
+  Function< char (char) > t = toto;
+  toto('c');
+
+  Function<int (const std::string&)> ft = boost::bind(&func, _1);
+  ft("Ça marche même avec un objet de type boost::bind !");
+  return 0;
 }
